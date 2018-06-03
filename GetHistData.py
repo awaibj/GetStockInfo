6e9687eec6933f97029b829ecef1ec209747d1ed
awaibj <sifeng04@126.com>
2018-06-04 00:15:06 +0800

    add pd1 to pd

--- GetHistData.py
+++ GetHistData.py
@@ -8,11 +8,23 @@ HistDataName='HistData.csv'
 StartDate='2018-01-01'
 EndDate='2018-06-03'
 
+# 使用pandas读取，股票代码前面的零被去掉了
 # BasicDf=pd.DataFrame.from_csv(BasicInfoName,encoding='gbk')
-BasicDf=pd.read_csv(BasicInfoName,encoding='gbk')
-a=BasicDf['code']
-print a.shape
-for code in BasicDf['code']:
-    pd1=ts.get_hist_data(code,StartDate,EndDate)
-    print pd1
-    break
+# BasicDf=pd.read_csv(BasicInfoName,encoding='gbk')
+# a=BasicDf['code']
+# print a.shape
+fp=open(BasicInfoName,mode='r')
+line=fp.readline()
+line=fp.readline()
+code = line.split(',')[0]
+# for code in BasicDf['code']:
+df=pd.DataFrame()
+while code != '':
+    df1=ts.get_hist_data(code,StartDate,EndDate)
+    data2 = [code for i in range(df1.shape[0])]
+    df1['code']=data2
+    # df=df.append(df1,ignore_index=True)
+    df = df.append(df1)
+    line = fp.readline()
+    code = line.split(',')[0]
+
