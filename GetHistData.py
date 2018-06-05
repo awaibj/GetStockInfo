a5e5e0071e9b6502b2efb6c4b4d7ccb77cc8bbe0
awaibj <sifeng04@126.com>
2018-06-05 21:13:12 +0800

    save stock data to csv file

--- GetHistData.py
+++ GetHistData.py
@@ -21,10 +21,20 @@ code = line.split(',')[0]
 df=pd.DataFrame()
 while code != '':
     df1=ts.get_hist_data(code,StartDate,EndDate)
-    data2 = [code for i in range(df1.shape[0])]
-    df1['code']=data2
-    # df=df.append(df1,ignore_index=True)
-    df = df.append(df1)
+    # 300750 is None
+    if isinstance(df1, pd.DataFrame): #df1 != None:
+        data2 = [code for i in range(df1.shape[0])]
+        df1['code']=data2
+        list1=df1.columns
+        list2=[]
+        list2.append(list1[-1])
+        list2.extend(list1[0:-1])
+        df1=df1.reindex(columns=list2)
+        # df=df.append(df1,ignore_index=True)
+        df = df.append(df1)
     line = fp.readline()
     code = line.split(',')[0]
+    print code
+#save to file
+df.to_csv(HistDataName,encoding='gbk')
 
